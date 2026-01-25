6619c17c08b4216f07fffdd544bfd0dc3e045ed1
tsite <stubydent@gmail.com>
2026-01-25 02:28:27 -0800

    add utf-8 pathcache test

---
+++ tests/pathcache-utf8-bitmap/test.rb
@@ -0,0 +1,9 @@
+# Test suite for mkxp-z.
+# License GPLv2+.
+#
+# Run the suite via the "customScript" and "gameFolder" fields in mkxp.json
+# with "pathCache" set to true.
+
+Bitmap.new("l.M\xc3\xa9") # nfc
+System::puts("Test passed - done")
+exit
